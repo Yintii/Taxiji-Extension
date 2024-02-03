718ec62f6839b437dd8d4aeb7e228fe93032a67e
yintii <keleheart1@gmail.com>
2024-02-02 17:34:17 -0800

    pushing to show progress on issue of connecting to the main app

--- popup.tsx
+++ popup.tsx
@@ -5,7 +5,48 @@ function IndexPopup() {
 
   const [user, setUser] = useState(null)
 
+  const url = "https://nameless-thicket-51908-7757c6d88739.herokuapp.com/"
+  const cookie = 'user_wallets'
 
+  const checkUser = async () => {
+    chrome.cookies.get({ url: url, name: cookie }, function (cookie) {
+      if (cookie) {
+        try {
+
+            //get the user_info cookie
+            const user_info = cookie.value
+            console.log(user_info)
+
+          // Implement your logic to extract user information from the decoded value
+        } catch (error) {
+          console.error('Error decoding cookie value:', error);
+        }
+      } else {
+        console.log('Cookie not found');
+      }
+    });
+
+  }
+
+
+  // function parseUserInfo(decodedValue) {
+  //   // Implement your parsing logic based on the actual format used by the server
+  //   // For example, if the format is key-value pairs separated by some delimiter:
+  //   var keyValuePairs = decodedValue.split('&');
+  //   var userInfo = {};
+
+  //   keyValuePairs.forEach(function (pair) {
+  //     var [key, value] = pair.split('=');
+  //     userInfo[key] = value;
+  //   });
+
+  //   return userInfo;
+  // }
+
+
+  useEffect(() => {
+    checkUser()
+  }, [])
 
   const Default = () => {
     return(
@@ -27,7 +68,7 @@ function IndexPopup() {
 
   return (
     <div>
-      {user ? <Default /> : <Login setUser={setUser} />}
+      {user ? <Default /> : <Login />}
     </div>
   )
 }
